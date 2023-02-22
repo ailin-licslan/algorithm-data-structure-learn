50db67a3abbdfd8fdda9f4676881c962478137c3
weilin <weilin.huang@renren-inc.com>
2023-02-22 12:14:18 +0800

    happy number

--- src/com/licslan/interview/leetcode500/basestructure/Add2Num02.java
+++ src/com/licslan/interview/leetcode500/basestructure/Add2Num02.java
@@ -165,6 +165,35 @@ public class Add2Num02 {
     }
 
 
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+
+
+
     //public static void main(String[] args) {
 
 //        //[8,2,3]    [9999000]
