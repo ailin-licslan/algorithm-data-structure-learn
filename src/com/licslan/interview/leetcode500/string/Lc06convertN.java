5b4aceafc1f681bbc7ff12652280c46afea9158e
licslan <licslan@sina.com>
2023-03-21 13:40:03 +0800

    LC06

---
+++ src/com/licslan/interview/leetcode500/string/Lc06convertN.java
@@ -0,0 +1,44 @@
+package com.licslan.interview.leetcode500.string;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * 6. N 字形变换
+ * 中等
+ * 2K
+ * 相关企业
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+ *
+ * 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+ *
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+ *
+ * 请你实现这个将字符串进行指定行数变换的函数：
+ *
+ * string convert(string s, int numRows);
+ * */
+public class Lc06convertN {
+    public static String convert(String s, int numRows) {
+        if(numRows < 2) return s;
+        List<StringBuilder> rows = new ArrayList<StringBuilder>();
+        for(int i = 0; i < numRows; i++) rows.add(new StringBuilder());
+        int i = 0, flag = -1;
+        for(char c : s.toCharArray()) {
+            rows.get(i).append(c);
+            if(i == 0 || i == numRows -1) flag = - flag;
+            i += flag;
+        }
+        StringBuilder res = new StringBuilder();
+        for(StringBuilder row : rows) res.append(row);
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(convert("LEETCOD",3));
+    }
+
+}
