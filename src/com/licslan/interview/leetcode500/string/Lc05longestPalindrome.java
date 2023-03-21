65b4648b72310e735a79cd480f0fd93c988b344b
licslan <licslan@sina.com>
2023-03-21 13:15:44 +0800

    lc05

---
+++ src/com/licslan/interview/leetcode500/string/Lc05longestPalindrome.java
@@ -0,0 +1,51 @@
+package com.licslan.interview.leetcode500.string;
+/**
+ * 5. 最长回文子串
+ * 提示
+ * 中等
+ * 6.3K
+ * 相关企业
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ *
+ * 如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * 示例 2：
+ *
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ * */
+public class Lc05longestPalindrome {
+
+    //中心扩散法
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return right - left - 1;
+    }
+}
