66f18c57ce2c75ebadcb44cc560bfcc12b35131b
licslan <licslan@sina.com>
2023-05-31 12:20:31 +0800

    group

--- src/com/licslan/group/TestV2.java
+++ src/com/licslan/group/TestV2.java
@@ -1,13 +1,13 @@
 package com.licslan.group;
 
-/* Online Java Compiler and Editor */
+
 import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author Lin
  */
-public class Test {
+public class TestV2 {
     public static List<Map<Integer, List<String>>> groupNames(List<String> nameList) {
         // Shuffle the name list to randomize the order
         Collections.shuffle(nameList);
@@ -72,7 +72,7 @@ public class Test {
 
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Current people Num is ===> ");
+        System.out.println("本次参加的人数是：");
 
         String [] num = new String[1];
         num[0]=scanner.nextLine();
@@ -85,9 +85,9 @@ public class Test {
 
 
         Scanner scanner2 = new Scanner(System.in);
-        System.out.println("The name is ====>");
+        System.out.println("名称分别是：");
 
-        for(int i=0; i<lng; i++) {
+        for(int i=0;i<lng;i++){
             line[i]=scanner2.nextLine();
         }
         List<String> nameList = Arrays.asList(line);
@@ -95,32 +95,55 @@ public class Test {
         scanner.close();
         scanner2.close();
 
+//        String fromWechat = "1. 群昵称/微信名\n" +
+//                "2. Lighters\n" +
+//                "3. Ada\n" +
+//                "4. Saltie\n" +
+//                "5. Amanda\n" +
+//                "6. sunny\n" +
+//                "7. seika\n" +
+//                "8. popcorn\n" +
+//                "9. Gabby\n" +
+//                "10. Sarah.C\n" +
+//                "11. Cici\n" +
+//                "12. DAISY\n" +
+//                "13. AKI";
+//
+//        String[] split = fromWechat.split("\\n");
+//        List<String> nameList = new ArrayList<>(split.length);
+//        for (String s : split) {
+//            String s1 = s.split(". ")[1];
+//            nameList.add(s1);
+//        }
+
+
         List<String> roomList = Arrays.asList(
                 "G1: 478-8861-9301\n" ,
-                "G2: 410-4417-3296\n" ,
-                "G3: 756-6799-0054\n" ,
-                "G4: 498-8362-1185\n" ,
-                "G5: 996-9781-1440\n" ,
-                "G6: 967-0700-2669\n" ,
-                "G7: 928-792-991\n" ,
-                "G8: 868-0177-7516\n" ,
-                "G9: 905-4432-6019\n" ,
-                "G10: 334-6750-8975\n" ,
-                "G11: 777-5534-5107\n" ,
-                "G12: 524-3216-4842\n" ,
-                "G13: 839-3059-3701\n" ,
-                "G14: 824-7677-4422\n" ,
-                "G15: 738-5844-3061\n");
+                        "G2: 410-4417-3296\n" ,
+                        "G3: 756-6799-0054\n" ,
+                        "G4: 498-8362-1185\n" ,
+                        "G5: 996-9781-1440\n" ,
+                        "G6: 967-0700-2669\n" ,
+                        "G7: 928-792-991\n" ,
+                        "G8: 868-0177-7516\n" ,
+                        "G9: 905-4432-6019\n" ,
+                        "G10: 334-6750-8975\n" ,
+                        "G11: 777-5534-5107\n" ,
+                        "G12: 524-3216-4842\n" ,
+                        "G13: 839-3059-3701\n" ,
+                        "G14: 824-7677-4422\n" ,
+                        "G15: 738-5844-3061\n");
         List<Map<Integer, List<String>>> groups = groupNames(nameList);
 
         for (Map<Integer, List<String>> group : groups) {
             for (Map.Entry<Integer, List<String>> entry : group.entrySet()) {
                 int groupIndex = entry.getKey();
                 List<String> names = entry.getValue();
-                System.out.println("The " + groupIndex + " Group people is : " + names + "  The Room No is:  " + roomList.get(groupIndex - 1));
+                System.out.print("第 " + groupIndex + " 组参与的人分别是: " + names + "  The Room No is:  " + roomList.get(groupIndex - 1));
             }
         }
     }
 
 
 }
+
